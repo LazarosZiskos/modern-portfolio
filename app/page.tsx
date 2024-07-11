1971d1cc79bc47e6b48f6e8286ad6a766ba086da
ΛΑΖΑΡΟΣ ΖΗΣΚΟΣ <ziskoslaz@gmail.com>
2024-07-11 15:58:26 +0300

    initilize project

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Hero from "@/components/Hero";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main
+      className="relative bg-black-100 flex justify-center flex-col items-center
+    overflow-hidden mx-auto px-5 sm:px-10"
+    >
+      <div className="max-w-7xl w-full">
+        <Hero />
+      </div>
+    </main>
+  );
+}
